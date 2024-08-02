0debb0338b6e7d990c49ca76a47aee9e7a856969
Leandro Foly <lefoly@gmail.com>
2024-08-02 18:55:53 -0300

    atual

---
+++ ExemploMestreDetalhes/src/main/java/model/Turma.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author lefoly
+ */
+public class Turma {
+
+    private int id;
+    private String nome;
+    private List<Aluno> alunos;
+
+    public Turma() {
+    }
+
+    public Turma(String nome, List<Aluno> alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    @Override
+    public String toString() {
+        return "Turma{" + nome + ", alunos=" + alunos + '}';
+    }    
+}
